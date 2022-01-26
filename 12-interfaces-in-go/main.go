218be45cd49c89e41adab02c8a6b9d5e589d0204
Jesse Neal <jesse.neal@hashicorp.com>
2022-01-26 17:11:30 -0600

    adding methods and interfaces

---
+++ 12-interfaces-in-go/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Employee interface {
+	GetName() string
+}
+
+type Engineer struct {
+	Name string
+}
+
+func (e *Engineer) GetName() string {
+	return "Employee Name: " + e.Name
+}
+
+type Manager struct {
+	Name string
+}
+
+func (m *Manager) GetName() string {
+	return "Manager Name: " + m.Name
+}
+
+func PrintDetails(e Employee) {
+	fmt.Println(e.GetName())
+}
+
+func main() {
+	engineer := &Engineer{Name: "Jesse"}
+	manager := &Manager{Name: "Donna"}
+	PrintDetails(engineer)
+	PrintDetails(manager)
+}
